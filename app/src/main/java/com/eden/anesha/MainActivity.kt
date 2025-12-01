2cc2dd97ce73b0c78ba955cc10a7f767b7a937c0
EdenMD <techn2533@gmail.com>
2025-12-01 12:13:22 +0200

    Implement WebView initialization and load URL

--- app/src/main/java/com/eden/anesha/MainActivity.kt
+++ app/src/main/java/com/eden/anesha/MainActivity.kt
@@ -2,10 +2,26 @@ package com.eden.anesha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.webViewClient = WebViewClient() // This prevents opening links in an external browser
+        myWebView.settings.javaScriptEnabled = true // Enable JavaScript
+        myWebView.loadUrl("https://xbvercel.vercel.app") // Load your desired URL
+    }
+
+    override fun onBackPressed() {
+        val myWebView: WebView = findViewById(R.id.webview)
+        if (myWebView.canGoBack()) {
+            myWebView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
