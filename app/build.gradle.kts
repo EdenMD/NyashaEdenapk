29129d61667c6acf5efa2b03e5dbdb0a7631536e
EdenMD <techn2533@gmail.com>
2025-12-24 18:26:11 +0200

    Update build.gradle.kts
    fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-//----start of build_gradle_config----
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -7,90 +6,72 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-base {
-    // Naming APK // AAB
-    val timestamp = SimpleDateFormat("dd-MM-yyyy_hh-mm").format(Date())
-    archiveFileName = "${ProjectSetting.NAME_APK}-[${ProjectSetting.PROJECT_VERSION_NAME}]-$timestamp"
-}
+val timestamp = SimpleDateFormat("dd-MM-yyyy_hh-mm").format(Date())
 
 android {
     compileSdk = ProjectSetting.PROJECT_COMPILE_SDK
     namespace = ProjectSetting.PROJECT_NAME_SPACE
-
     defaultConfig {
-
         applicationId = ProjectSetting.PROJECT_APP_ID
         minSdk = ProjectSetting.PROJECT_MIN_SDK
         targetSdk = ProjectSetting.PROJECT_TARGET_SDK
         versionCode = ProjectSetting.PROJECT_VERSION_CODE
         versionName = ProjectSetting.PROJECT_VERSION_NAME
-
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
     }
-
     signingConfigs {
         create("release") {
-            // You need to specify either an absolute path or include the
-            // keystore file in the same directory as the build.gradle file.
-            // [PROJECT FOLDER NAME/app/[COPY YOUT KEY STORE].jks in here
             storeFile = file(ProjectSetting.KEY_PATH)
             storePassword = ProjectSetting.KEY_STORE_PASSWORD
             keyAlias = ProjectSetting.KEY_ALIAS
             keyPassword = ProjectSetting.KEY_ALIAS_PASSWORD
         }
     }
-
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-
-            // Disable Debug Mode
             isDebuggable = false
             isJniDebuggable = false
             isPseudoLocalesEnabled = false
-
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-
-            // Generated Signed APK / AAB
             signingConfig = signingConfigs.getByName("release")
-
         }
     }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        }
     }
-
+    applicationVariants.all {
+        outputs.all {
+            if (this is com.android.build.gradle.internal.api.ApkVariantOutputImpl) {
+                this.outputFileName = "${ProjectSetting.NAME_APK}-[${ProjectSetting.PROJECT_VERSION_NAME}]-${timestamp}.apk"
+            }
+        }
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.material)
-    implementation("com.google.android.gms:play-services-ads:23.0.0") // AdMob dependency
-
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
-//----end of build_gradle_config----
